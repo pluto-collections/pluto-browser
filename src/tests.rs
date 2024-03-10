f439483d5222ab74a4afe99d7e45aa798fa712f1
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-03-10 21:02:06 +0545

    add test for add_http_to_entry function

--- src/tests.rs
+++ src/tests.rs
@@ -18,3 +18,23 @@ fn get_search_type_test() {
         assert_eq!(search_type, &get_search_type(&string.to_string()));
     }
 }
+
+#[test]
+fn add_http_to_entry_test() {
+    let strings_with_http = [
+        ("https://www.google.com", "https://www.google.com"),
+        ("http://localhost:8000", "http://localhost:8000"),
+        ("google", "http://google"),
+        ("http://google.com", "http://google.com"),
+        ("https", "http://https"),
+        ("ftp://google.com", "http://ftp://google.com"),
+        ("http://google.com", "http://google.com"),
+    ];
+
+    for (string, http_string) in strings_with_http.iter() {
+        assert_eq!(
+            http_string,
+            &crate::utils::add_http_to_entry(&string.to_string())
+        );
+    }
+}
