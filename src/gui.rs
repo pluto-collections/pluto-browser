c19d56ae08d338653418016751416007b358e831
notsugat <sugatsujakhu@gmail.com>
2024-03-20 12:37:21 +0545

    patch: refractor code using clone after moving the value

--- src/gui.rs
+++ src/gui.rs
@@ -2,6 +2,7 @@ use crate::components::{
     browser, button,
     searchbar::{self, get_url},
 };
+use glib::clone;
 use gtk::prelude::*;
 use webkit2gtk::WebViewExt;
 
@@ -21,11 +22,7 @@ pub fn build_ui(application: &gtk::Application) {
     vbox.set_expand(true);
 
     let browser = browser::Browser::new();
-    let browser2 = browser.clone();
-    let browser_clone = browser.clone();
-    let browser_clone2 = browser.clone();
     let searchbar = searchbar::SearchBar::new();
-    let searchbar_clone = searchbar.clone();
     let previous_button = button::WebViewButton::new(Some("go-previous"));
     let next_button = button::WebViewButton::new(Some("go-next"));
 
@@ -35,32 +32,35 @@ pub fn build_ui(application: &gtk::Application) {
     vbox.add(browser.get_widget());
 
     // Connect the button to the browser
-    previous_button.button.connect_clicked(move |_| {
-        browser_clone.get_widget().go_back();
-        println!("Going back");
-    });
+    previous_button
+        .button
+        .connect_clicked(clone!(@strong browser => move |_| {
+            browser.get_widget().go_back();
+        }));
 
     // Connect the button to the browser
-    next_button.button.connect_clicked(move |_| {
-        browser_clone2.clone().get_widget().go_forward();
-        println!("Going forward");
-    });
+    next_button
+        .button
+        .connect_clicked(clone!(@strong browser => move |_| {
+            browser.get_widget().go_back();
+        }));
 
     // Connect the searchbar to the browser
-    searchbar_clone.get_widget().connect_activate(move |entry| {
-        let uri = entry.text();
-        let uri = get_url(&uri.to_string());
-        browser.clone().update_uri(&uri);
-    });
+    searchbar
+        .get_widget()
+        .connect_activate(clone!(@strong browser =>  move |entry| {
+            let uri = entry.text();
+            let uri = get_url(&uri.to_string());
+            browser.clone().update_uri(&uri);
+        }));
 
     // update the uri when the webview changes
-    let browser_clone = browser2.clone();
-    browser_clone
+    browser
         .get_widget()
-        .connect_uri_notify(move |webview| {
+        .connect_uri_notify(clone!(@strong browser  => move |webview| {
             let uri = webview.uri().unwrap();
             searchbar.get_widget().set_text(&uri);
-        });
+        }));
 
     // Show all widgets
     window.show_all();
