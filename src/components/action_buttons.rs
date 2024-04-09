b29ffeb9cdceb57b641a0fb77a522466bd35321b
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2024-04-09 07:21:46 +0545

    change reload button to x button when page is loading

--- src/components/action_buttons.rs
+++ src/components/action_buttons.rs
@@ -1,20 +1,22 @@
 use super::{browser::Browser, button};
 use glib::clone;
 use gtk::prelude::{BoxExt, ButtonExt, ContainerExt};
+use std::cell::Cell;
 use std::rc::Rc;
-use webkit2gtk::WebViewExt;
+use webkit2gtk::{LoadEvent, WebViewExt};
 
 pub struct ActionButtons {
-    previous_button: button::WebViewButton,
-    next_button: button::WebViewButton,
-    refresh_button: button::WebViewButton,
+    previous_button: Rc<button::WebViewButton>,
+    next_button: Rc<button::WebViewButton>,
+    refresh_button: Rc<button::WebViewButton>,
+    is_refreshing: Rc<Cell<bool>>,
 }
 
 impl ActionButtons {
     pub fn new(_css_provider: Rc<gtk::CssProvider>) -> Self {
-        let previous_button = button::WebViewButton::new(Some("go-previous"));
-        let next_button = button::WebViewButton::new(Some("go-next"));
-        let refresh_button = button::WebViewButton::new(Some("view-refresh"));
+        let previous_button = Rc::new(button::WebViewButton::new(Some("go-previous")));
+        let next_button = Rc::new(button::WebViewButton::new(Some("go-next")));
+        let refresh_button = Rc::new(button::WebViewButton::new(Some("view-refresh")));
 
         let vbox = gtk::Box::new(gtk::Orientation::Vertical, 0);
         let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 0);
@@ -22,6 +24,7 @@ impl ActionButtons {
         vbox.add(&hbox);
 
         ActionButtons {
+            is_refreshing: Rc::new(Cell::new(false)),
             previous_button,
             next_button,
             refresh_button,
@@ -51,10 +54,31 @@ impl ActionButtons {
                 browser.get_widget().go_forward();
             }));
 
+        let is_refreshing = Rc::clone(&self.is_refreshing);
         self.refresh_button
             .button
             .connect_clicked(clone!(@strong browser => move |_| {
-                browser.get_widget().reload();
+                if is_refreshing.get() {
+                    browser.get_widget().stop_loading();
+                } else {
+                    browser.get_widget().reload();
+                }
             }));
+
+        let refresh_button = Rc::clone(&self.refresh_button);
+        let is_refreshing = Rc::clone(&self.is_refreshing);
+        browser.get_widget().connect_load_changed(
+            move |_, load_event: LoadEvent| match load_event {
+                LoadEvent::Started => {
+                    refresh_button.set_icon_name("process-stop");
+                    is_refreshing.set(true);
+                }
+                LoadEvent::Committed => {
+                    refresh_button.set_icon_name("view-refresh");
+                    is_refreshing.set(false);
+                }
+                _ => {}
+            },
+        );
     }
 }
--- src/components/button.rs
+++ src/components/button.rs
@@ -1,3 +1,5 @@
+use gtk::prelude::ButtonExt;
+
 #[derive(Debug)]
 pub struct WebViewButton {
     pub button: gtk::Button,
@@ -8,5 +10,12 @@ impl WebViewButton {
         Self {
             button: gtk::Button::from_icon_name(label, gtk::IconSize::Button),
         }
+    }
+
+    pub fn set_icon_name(&self, icon_name: &str) {
+        self.button.set_image(Some(&gtk::Image::from_icon_name(
+            Some(icon_name),
+            gtk::IconSize::Button,
+        )));
     }
 }
